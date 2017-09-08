981f75153c9cc4e24785b5d4c0551013f0d2f4f9
galo <galo.mora@gmail.com>
2017-09-08 16:53:44 -0500

    Notifications

---
+++ box-web-client/src/app/notification.options.ts
@@ -0,0 +1,21 @@
+export class NotificationOptions {
+//    options: {
+//        position: Array<string>,
+//        timeOut: number,
+//        lastOnBottom: boolean
+//    }
+    
+    static options = {
+            position: ['top', 'right'],
+            timeOut: 5000,
+            lastOnBottom: true
+        };
+    
+//    constructor() {
+//        this.options = {
+//            position: ["top", "right"],
+//            timeOut: 5000,
+//            lastOnBottom: true
+//        }
+//    }
+}
